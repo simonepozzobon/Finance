3bf7332538ecc5de6409bda6f06f45bd51f7d056
simone pozzobon <simonepozzobon@Esse-002.homenet.telecomitalia.it>
2017-03-15 17:38:45 +0100

    Made Blade Layout

---
+++ resources/views/layouts/main/_head.blade.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <title>Gestionale - @yield('title')</title>
+
+    @yield('stylesheets')
+</head>
---
+++ resources/views/layouts/main/_sidebar.blade.php
@@ -0,0 +1,3 @@
+<ul class="nav nav-pills flex-column">
+   <li class="nav-item"><a href="#">Item</a></li>
+ </ul>
